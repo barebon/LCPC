9d56ba30fa2060cdeef228218ef712b05d7deb66
Cian Finnegan <cianfinnegan@hotmail.com>
2019-06-14 13:23:50 +0100

    Added support for:
    LCVP
    Foundation level maths & Irish
    Answers in Irish
    More flexibility in answers
    
    Began framework for supporting answers with accents.
    
    & General tidying

--- LeavingCertificatePointsCalculator.py
+++ LeavingCertificatePointsCalculator.py
@@ -1,43 +1,94 @@
 #Global Array:
-med_points = 550 #Current points for med
-Ordinary = ["ordinary", "ol", "ordinary level", "pass", "pass level"]
-High_Grade_Names = ["h8", "h7", "h6" ,"h5" ,"h4", "h3", "h2", "h1"]
-High_Grade_Points = [0, 37, 46, 56, 66, 77, 88, 100]
-Ord_Grade_Names = ["o8", "o7", "o6" ,"o5" ,"o4", "o3", "o2", "o1"]
-Ord_Grade_Points = [0, 0, 12, 20, 28, 37, 46, 56]
-Maths_Names = ["maths", "math", "mathematic", "mathematics", "arithmetic"]
+
+higher_level = ["h", "a", "hl", "al", "higher", "higher level", "honours", "honours level", "honors" "honors level", "hons", "ardleibheal", "ard"]
+ordinary_level = ["o", "p", "g", "ol", "gl", "ordinary", "ordinary level", "pass", "pass level", "gnath", "gnathleibheal",]
+foundation_level = ["f", "b", "fl", "bl", "foundation", "foundation level", "bonn", "bonnleibheal"]
+
+higher_grades = ["h8", "h7", "h6" ,"h5" ,"h4", "h3", "h2", "h1"]
+higher_grade_points = [0, 37, 46, 56, 66, 77, 88, 100]
+
+ordinary_grades = ["o8", "o7", "o6" ,"o5" ,"o4", "o3", "o2", "o1"]
+ordinary_grade_points = [0, 0, 12, 20, 28, 37, 46, 56]
+
+foundation_grades = ["f2", "f1"]
+foundation_grade_points = [12, 20]
+
+common_grades = ["ungraded", "fail", "pass", "merit", "distinction"]
+common_grade_points = [0, 0, 28, 46, 66]
+
+maths = ["maths", "math", "mathematic", "mathematics", "arithmetic", "matamaitic", "mata",]
+irish = ["irish", "gaeilge"]
+lcvp = ["lcvp", "leaving certificate vocational program", "leaving cert vocational programme", "leaving certificate vocational program", "leaving certificate vocational programme"]
+
 point_list = []
 shortlist = []
 total_points = 0
 
-Number_Subjects = int(input("How many subjects did you sit for your Leaving Certificate? Please input as a number (e.g. \"1\" not \"one\"): "))
-for i in range(Number_Subjects):
-    Subject = input("Name a subject you took for the Leaving Certificate. ")
+import unicodedata
+def remove_diacritic(input):
+    return unicodedata.normalize('NFKD', input).encode('ASCII', 'ignore')
 
-    level = str.lower(input("Did you sit Higher or Ordinary Level {}? ".format(Subject)))
-    if level in Ordinary:
-        Ord_Grade = str.lower(input("What grade did you achieve in Ordinary Level {}? ".format(Subject)))
-        for grade in Ord_Grade_Names:
-            if grade == Ord_Grade:
-                points = Ord_Grade_Points[Ord_Grade_Names.index(grade)]
-                print("You receieved {} points from this subject.".format(str(points)))
-                #An alternative method to this is print("From this subject, your points were " + str(points))
-                point_list.append(points)
+number_of_subjects = int(input("How many subjects did you sit for your Leaving Certificate? Please input as a number (e.g. \"1\" not \"one\"): "))
+for i in range(number_of_subjects):
+
+
+    subject = input("Name a subject you took for the Leaving Certificate. ")
+    lowered_subject = str.lower(subject)
+
+    if lowered_subject in lcvp:
+        level = "common"
+        sanitised_level = level
 
     else:
-        High_Grade = str.lower(input("What grade did you achieve in Higher Level {}? ".format(Subject)))
-        for grade in High_Grade_Names:
-            if grade == High_Grade:
-                points = High_Grade_Points[High_Grade_Names.index(grade)]
-                if str.lower(Subject) in Maths_Names:
+        if lowered_subject in irish or lowered_subject in maths:
+            level = str.lower(input("Did you sit Higher, Ordinary or Foundation Level {}? ".format(subject)))
+            sanitised_level = level
+
+        else:
+            level = str.lower(input("Did you sit Higher or Ordinary Level {}? ".format(subject)))
+            sanitised_level = level
+
+
+    if sanitised_level in higher_level:
+        higher_grade = str.lower(input("What grade did you achieve in Higher Level {}? ".format(subject)))
+        for grade in higher_grades:
+            if grade == higher_grade:
+                points = higher_grade_points[higher_grades.index(grade)]
+                if str.lower(subject) in maths:
                     if points >= 46:
                         points = points + 25
                 print("You receieved {} points from this subject.".format(str(points)))
-                #An alternative method to this is print("From this subject, your points were " + str(points))
+                #Alternative: print("From this subject, your points were " + str(points))
+                point_list.append(points)
+
+    if sanitised_level in ordinary_level:
+        ordinary_grade = str.lower(input("What grade did you achieve in Ordinary Level {}? ".format(subject)))
+        for grade in ordinary_grades:
+            if grade == ordinary_grade:
+                points = ordinary_grade_points[ordinary_grades.index(grade)]
+                print("You receieved {} points from this subject.".format(str(points)))
+                #Alternative: print("From this subject, your points were " + str(points))
+                point_list.append(points)
+
+    if sanitised_level in foundation_level:
+        foundation_grade = str.lower(input("What grade did you achieve in Foundation Level {}? ".format(subject)))
+        for grade in foundation_grades:
+            if grade == foundation_grade:
+                points = foundation_grade_points[foundation_grades.index(grade)]
+                print("You receieved {} points from this subject.".format(str(points)))
+                #Alternative: print("From this subject, your points were " + str(points))
+                point_list.append(points)
+
+    if sanitised_level == "common":
+        common_grade = str.lower(input("What grade did you achieve in {}? ".format(subject)))
+        for grade in common_grades:
+            if grade == common_grade:
+                points = common_grade_points[common_grades.index(grade)]
+                print("You receieved {} points from this subject.".format(str(points)))
+                #Alternative: print("From this subject, your points were " + str(points))
                 point_list.append(points)
 
 point_list.sort(reverse = True)
 shortlist = point_list[:6]
-print(point_list)
-print(shortlist)
-print("Total Points: " + sum(shortlist))
+total_points = sum(shortlist)
+print("Total Points:", total_points)
